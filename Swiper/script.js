5eb08e8ee211594aa83736414daaa80c95b0df5f
Emil <kotaravolti46@gmail.com>
2026-02-19 20:51:10 +0900

    fix script.js

--- Swiper/script.js
+++ Swiper/script.js
@@ -1,12 +1,32 @@
 if (window.innerWidth <= 767) {
-
-  new Swiper('.brands__swiper', {
-    slidesPerView: 'auto',
+  new Swiper(".brands__swiper", {
+    slidesPerView: 1.2,
     spaceBetween: 16,
     pagination: {
-      el: '.swiper-pagination',
+      el: ".swiper-pagination",
       clickable: true,
     },
   });
+}
 
+const toggleBtn = document.querySelector(".js-toggle-btn");
+const list = document.querySelector(".brands__list");
+
+toggleBtn.addEventListener("click", () => {
+  const isExpanded = list.classList.toggle("expanded");
+
+  toggleBtn.textContent = isExpanded
+    ? "Скрыть"
+    : "Показать все";
+});
+
+if (window.innerWidth <= 767) {
+  new Swiper(".brands_swiper", {
+    slidesPerView: "auto",
+    spaceBetween: 16,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 }
