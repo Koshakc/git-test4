39f4fa9ccb60d1dcff3062c7234a3a78465e353c
Emil <kotaravolti46@gmail.com>
2026-03-03 21:02:13 +0900

    init show more

--- Swiper/script.js
+++ Swiper/script.js
@@ -1,32 +1,14 @@
-if (window.innerWidth <= 767) {
-  new Swiper(".brands__swiper", {
-    slidesPerView: 1.2,
-    spaceBetween: 16,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+document.addEventListener("DOMContentLoaded", function () {
+
+  const button = document.querySelector(".brands_button");
+  const list = document.querySelector(".brands_list");
+
+  button.addEventListener("click", () => {
+    const isExpanded = list.classList.toggle("expanded");
+
+    button.textContent = isExpanded
+      ? "Скрыть"
+      : "Показать все";
   });
-}
 
-const toggleBtn = document.querySelector(".js-toggle-btn");
-const list = document.querySelector(".brands__list");
-
-toggleBtn.addEventListener("click", () => {
-  const isExpanded = list.classList.toggle("expanded");
-
-  toggleBtn.textContent = isExpanded
-    ? "Скрыть"
-    : "Показать все";
 });
-
-if (window.innerWidth <= 767) {
-  new Swiper(".brands_swiper", {
-    slidesPerView: "auto",
-    spaceBetween: 16,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
-}
