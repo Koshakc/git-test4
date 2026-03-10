ecfbf36bb9aa2a17b27c13b04110898d84f6f4d2
Emil <kotaravolti46@gmail.com>
2026-03-10 17:49:19 +0900

    fix mobile swiper

--- Swiper/script.js
+++ Swiper/script.js
@@ -1,14 +1,33 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
 
   const button = document.querySelector(".brands_button");
   const list = document.querySelector(".brands_list");
 
   button.addEventListener("click", () => {
-    const isExpanded = list.classList.toggle("expanded");
+    const expanded = list.classList.toggle("expanded");
 
-    button.textContent = isExpanded
+    button.textContent = expanded
       ? "Скрыть"
       : "Показать все";
+  });
+
+  new Swiper(".brands_swiper", {
+    slidesPerView: 1.3,
+    spaceBetween: 16,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+
+    breakpoints: {
+
+      768: {
+        enabled: false
+      }
+
+    }
+
   });
 
 });
